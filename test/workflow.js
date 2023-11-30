51d0a5f5d86f9c6d75120011c116199ee0d7e814
Baptiste Donaux <bdonaux@wanadev.fr>
2023-11-30 10:33:48 +0100

    test: update test according to current stdout log

--- test/workflow.js
+++ test/workflow.js
@@ -22,17 +22,10 @@ describe("Workflow", function() {
                         expect(log).to.match(/which rsync/);
                         expect(log).to.match(/which ssh/);
                         expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'which rsync || exit 1\'/);
-                        expect(log).to.match(/date \+\"%Y%d%m%H%M%S\"/);
+                        expect(log).to.match(/Release name/);
                         expect(log).to.match(/echo 'Local command'/);
-                        expect(log).to.match(/mkdir -p .*\/releases .*\/shared/);
-                        expect(log).to.match(/rsync -az -e 'ssh -p 22' --delete --delete-excluded  --exclude=file_to_share --exclude=folder_to_share .* test@localhost:.*\/releases\/\d{14}/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'mkdir -p .*\/shared\/folder_to_share\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'[ -L .*\/releases\/\d{14}\/folder_to_share ] || ln -s .*\/shared\/folder_to_share .*\/releases\/\d{14}\/folder_to_share\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'mkdir -p .*\/shared\/ && [ -e .*\/shared\/file_to_share ] || touch .*\/shared\/file_to_share && [ -L .*\/releases\/\d{14}\/file_to_share ] || ln -s .*\/shared\/file_to_share .*\/releases\/\d{14}\/file_to_share\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'cd .*\/releases\/\d{14} && echo \'Remote command\'\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'rm -f .*\/current && ln -s .*\/releases\/\d{14} .*\/current\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'cd .*\/releases\/\d{14} && echo \'Post deploy command\'\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'ls -1dt .*\/releases\/* | tail -n +4 | xargs rm -rf\'/);
+                        expect(log).to.match(/Plug folders\/files to share/);
+                        expect(log).to.match(/Copy release on remote hosts/);
 
                         done();
                     } catch (err) {
@@ -62,18 +55,12 @@ describe("Workflow", function() {
                         expect(log).to.match(/which rsync/);
                         expect(log).to.match(/which git/);
                         expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'which rsync || exit 1\'/);
-                        expect(log).to.match(/date \+\"%Y%d%m%H%M%S\"/);
-                        expect(log).to.match(new RegExp(`git clone -b v${pkg.version} https:\/\/github.com\/baptistedonaux\/bipbip.js.git --depth=1 \/tmp\/repository`));
+                        expect(log).to.match(/\d{14}/);
+                        expect(log).to.match(/Release name/);
+                        expect(log).to.match(/Initialization/);
                         expect(log).to.match(/echo 'Local command'/);
-                        expect(log).to.match(/mkdir -p \/home\/test\/repository\/releases \/home\/test\/repository\/shared/);
-                        expect(log).to.match(/rsync -az -e 'ssh -p 22' --delete --delete-excluded  --exclude=file_to_share --exclude=folder_to_share \/tmp\/repository\/ test@localhost:\/home\/test\/repository\/releases\/\d{14}/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'mkdir -p \/home\/test\/repository\/shared\/folder_to_share\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'[ -L \/home\/test\/repository\/releases\/\d{14}\/folder_to_share ] || ln -s \/home\/test\/repository\/shared\/folder_to_share \/home\/test\/repository\/releases\/\d{14}\/folder_to_share\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'mkdir -p \/home\/test\/repository\/shared\/ && [ -e \/home\/test\/repository\/shared\/file_to_share ] || touch \/home\/test\/repository\/shared\/file_to_share && [ -L \/home\/test\/repository\/releases\/\d{14}\/file_to_share ] || ln -s \/home\/test\/repository\/shared\/file_to_share \/home\/test\/repository\/releases\/\d{14}\/file_to_share\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'cd \/home\/test\/repository\/releases\/\d{14} && echo \'Remote command\'\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'rm -f \/home\/test\/repository\/current && ln -s \/home\/test\/repository\/releases\/\d{14} \/home\/test\/repository\/current\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'cd \/home\/test\/repository\/releases\/\d{14} && echo \'Post deploy command\'\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'ls -1dt \/home\/test\/repository\/releases\/* | tail -n +4 | xargs rm -rf\'/);
+                        expect(log).to.match(/Plug folders\/files to share/);
+                        expect(log).to.match(/Copy release on remote hosts/);
 
                         done();
                     } catch (err) {
@@ -99,20 +86,11 @@ describe("Workflow", function() {
                     try {
                         expect(log).to.match(/which git/);
                         expect(log).to.match(/which rsync/);
-                        expect(log).to.match(/date \+\"%Y%d%m%H%M%S\"/);
-                        expect(log).to.match(/git rev-parse --git-dir/);
-                        expect(log).to.match(/git reset --hard && git fetch --all && git fetch --tags/);
-                        expect(log).to.match(new RegExp(`git checkout -q refs/tags/v${pkg.version}`));
+                        expect(log).to.match(/Release name/);
+                        expect(log).to.match(/Initialization/);
                         expect(log).to.match(/echo 'Local command'/);
-                        expect(log).to.match(/mkdir -p \/home\/test\/repository\/releases \/home\/test\/repository\/shared/);
-                        expect(log).to.match(/rsync -az -e 'ssh -p 22' --delete --delete-excluded  --exclude=file_to_share --exclude=folder_to_share \/tmp\/repository\/ test@localhost:\/home\/test\/repository\/releases\/\d{14}/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'mkdir -p \/home\/test\/repository\/shared\/folder_to_share\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'[ -L \/home\/test\/repository\/releases\/\d{14}\/folder_to_share ] || ln -s \/home\/test\/repository\/shared\/folder_to_share \/home\/test\/repository\/releases\/\d{14}\/folder_to_share\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'mkdir -p \/home\/test\/repository\/shared\/ && [ -e \/home\/test\/repository\/shared\/file_to_share ] || touch \/home\/test\/repository\/shared\/file_to_share && [ -L \/home\/test\/repository\/releases\/\d{14}\/file_to_share ] || ln -s \/home\/test\/repository\/shared\/file_to_share \/home\/test\/repository\/releases\/\d{14}\/file_to_share\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'cd \/home\/test\/repository\/releases\/\d{14} && echo \'Remote command\'\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'rm -f \/home\/test\/repository\/current && ln -s \/home\/test\/repository\/releases\/\d{14} \/home\/test\/repository\/current\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'cd \/home\/test\/repository\/releases\/\d{14} && echo \'Post deploy command\'\'/);
-                        expect(log).to.match(/ssh -o StrictHostKeyChecking=no -p 22 test@localhost \'ls -1dt \/home\/test\/repository\/releases\/* | tail -n +4 | xargs rm -rf\'/);
+                        expect(log).to.match(/Plug folders\/files to share/);
+                        expect(log).to.match(/Copy release on remote hosts/);
 
                         done();
                     } catch (err) {
